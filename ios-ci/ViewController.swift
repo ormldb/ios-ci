b072b081695c81066bea51c1b0452f2cf32fed17
Or May-Paz <orm.ldb@gmail.com>
2018-08-21 16:27:22 +0300

    .gitignore + project files

---
+++ ios-ci/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-ci
+//
+//  Created by Or May-Paz on 21/08/2018.
+//  Copyright © 2018 Or May-Paz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
